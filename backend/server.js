89cc0479c0084c9803d16b317c332fa854d47bce
Natdanai <88823702+corodimas@users.noreply.github.com>
2023-07-31 07:38:18 +0700

    lastest

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const db = require('./models')
 const cors = require('cors')
+require("dotenv").config();
 
 const app = express();
 
@@ -17,11 +18,14 @@ app.use("/course",courseRouter)
 app.use("/employee",employeesRouter)
 app.use('/auth',UsersRouter)
 
-db.sequelize.sync().then(()=>
-{
-    app.listen(3001,()=>{
-        console.log("Server running on port 3001");
-    });
-    
-})
+db.sequelize
+.sync()
+.then(()=>{
+        app.listen(process.env.PORT || 3001, () =>{
+            console.log("Server running on port 3001")
+        }) 
+    }).catch((err)=>{
+        console.log(err);
+    })
+
 
